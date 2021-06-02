dbc747b80c4bf63ca2a7b2b874ce1eff1d938955
vfavorito <vince.favorito@gmail.com>
2021-06-02 18:29:59 -0500

    added favorite character component

--- smash/client/src/components/FavoriteCharacter/favoriteCharacter.jsx
+++ smash/client/src/components/FavoriteCharacter/favoriteCharacter.jsx
@@ -1,10 +1,23 @@
 import { React } from "react";
+import CharData from "../../utils/SmashCharacters.json";
+import "./favoriteCharacter.css"
 
 function FavoriteCharacter(){
-
+    console.log(CharData.characters)
     return(
-        //Choosing your favorite character will update the main dashboard to resemble your favorite character
+        <div>
         <h2>Favorite Character</h2>
+        <div className="container">
+        {CharData.characters.map(character => {
+            return(
+            <div className="charCard" key={character.id}>
+            <img alt={character.name} src={character.portrait} className="charPic" />
+            <p className="charText">{character.name}</p>
+            </div>
+            )
+        })}
+        </div>
+        </div>
     )
 }
 
--- smash/client/src/pages/MainDashboard/mainDashboard.jsx
+++ smash/client/src/pages/MainDashboard/mainDashboard.jsx
@@ -6,21 +6,44 @@ import TournamentInvites from "../../components/TournamentInvites/tournamentInvi
 import IronManInvites from "../../components/IronManInvites/ironManInvites";
 import FavoriteCharacter from "../../components/FavoriteCharacter/favoriteCharacter";
 import UserStats from "../../components/UserStats/userStats";
+import { Container, Row, Col } from "react-bootstrap";
 
-function MainDashboard(){
+function MainDashboard() {
     const { name, portrait } = useContext(UserContext);
-    return(
-        <div>
-        <h1>Main Dashboard</h1>
-        <h2>{name}</h2>
-        <img alt="portrait" src={portrait}></img>
-        <NavBar />
-        <FavoriteCharacter />
-        <FriendsList />
-        <TournamentInvites />
-        <IronManInvites />
-        <UserStats />
-        </div>
+    return (
+        <Container fluid>
+            <Row>
+                <h1>Main Dashboard</h1>
+            </Row>
+            <Row>
+                <Col sm={12} md={5}>
+                    <img alt="portrait" src={portrait}></img>
+                </Col>
+                <Col sm={12} md={5}>
+                    <h2>{name}</h2>
+                </Col>
+            </Row>
+            <Row>
+                <Col sm={12}>
+                    <NavBar />
+                </Col>
+            </Row>
+            <Row>
+                <Col sm={4}>
+                    <FavoriteCharacter className="favChar"/>
+                </Col>
+                <Col sm={6}>
+                    <FriendsList />
+                </Col>
+            </Row>
+            <Row>
+                <Col sm={12}>
+                    <TournamentInvites />
+                    <IronManInvites />
+                    <UserStats />
+                </Col>
+            </Row>
+        </Container>
     )
 }
 export default MainDashboard;
