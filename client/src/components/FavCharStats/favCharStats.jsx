5c6f1e866555bc73c49451d989eb2e784ccba682
vfavorito <vince.favorito@gmail.com>
2021-08-27 14:29:35 -0500

    adjusted sizing for character stats

--- client/src/components/FavCharStats/favCharStats.jsx
+++ client/src/components/FavCharStats/favCharStats.jsx
@@ -7,10 +7,10 @@ function FavCharStats(props) {
         console.log(props.stats,"props.stats")
         return (
             <div id="charCard">
-                <h2 >{props.stats.quote}</h2>
+                <h4>{props.stats.quote}</h4>
                 <img id="charImg" src={props.stats.portrait} alt="character portrait" />
-                <h3>Wins: {props.stats.stats.wins}</h3>
-                <h3>Loses: {props.stats.stats.losses}</h3>
+                <h5>Wins: {props.stats.stats.wins}</h5>
+                <h5>Loses: {props.stats.stats.losses}</h5>
             </div>
         )
     }
