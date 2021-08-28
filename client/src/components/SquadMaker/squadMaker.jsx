7166cf706f5488d8059dc6e951e561f1c3f709b3
vfavorito <vince.favorito@gmail.com>
2021-08-27 20:01:06 -0500

    trying to fix vote bug

--- client/src/components/SquadMaker/squadMaker.jsx
+++ client/src/components/SquadMaker/squadMaker.jsx
@@ -110,7 +110,7 @@ function SquadMaker() {
                             if (res.data.vote.yays + res.data.vote.nays === res.data.participants.length - 1) {
                                 if (res.data.vote.nays === 0 && permanentName.current === res.data.admin) {
                                     updater(res.data.roundWinner);
-                                    API.updateArena(lobbyCode, { vote: { voteOpen: false, yays: 0, nays: 0 } })
+                                    await API.updateArena(lobbyCode, { vote: { voteOpen: false, yays: 0, nays: 0 } })
                                     setVoteModalIsOpen(false)
                                 }
                                 else {
